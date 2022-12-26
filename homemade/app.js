7ddaf0bb829277da72c4c7ff916df997ae3f3a92
Kohta9521 <kohta0421kochi@gmail.com>
2022-12-26 21:52:23 +0900

    add json file and add body area

--- homemade/app.js
+++ homemade/app.js
@@ -6,8 +6,10 @@ const port = 3000;
 app.use(express.json());
 
 app.post("/booklog", (req, res) => {
+    const booklog = req.body
     res.json({
-        "OK": true
+        "OK": true,
+        "booklog": booklog
     })
 });
 
